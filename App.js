04c3457bd8471e3f97032f270fe976272d3482a1
hayoung9000 <vitamin.char@gmail.com>
2019-09-25 22:47:08 +0900

    프로젝트 생성

---
+++ App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Timer from "./components/timer";
+
+export default class App extends Component {
+  render() {
+    return <Timer />;
+  }
+}
