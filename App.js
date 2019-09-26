7ffeeac2038494286a779ca0a45504ffe4943dfa
hayoung9000 <vitamin.char@gmail.com>
2019-09-26 18:42:39 +0900

    Components에 State 연결하기

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Timer from "./components/timer";
+import reducer from "./reducer";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+let store = createStore(reducer);
+
+console.log(store.getState());
 
 export default class App extends Component {
   render() {
-    return <Timer />;
+    return (
+      <Provider store={store}>
+        <Timer />
+      </Provider>
+    );
   }
 }
