280ea9fe7efe38984534613681f754ed6aaa78ce
hayoung9000 <vitamin.char@gmail.com>
2019-09-26 18:13:46 +0900

    Button Components 추가

--- components/timer/index.js
+++ components/timer/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View, StatusBar } from "react-native";
+import Button from "../button";
 
 export default class Timer extends Component {
   render() {
@@ -10,7 +11,8 @@ export default class Timer extends Component {
           <Text style={styles.time}>25:00</Text>
         </View>
         <View style={styles.lower}>
-          <Text>BUTTON</Text>
+          <Button iconName="play-circle" onPress={() => alert("it works")} />
+          <Button iconName="stop-circle" onPress={() => alert("it works")} />
         </View>
       </View>
     );
